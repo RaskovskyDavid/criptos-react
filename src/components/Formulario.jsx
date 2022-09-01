53e3b19befc796a57c06d1f9f6f6589094195bff
david <davidtucu@hotmail.com>
2022-09-01 17:12:34 -0300

    Hooks personalizados

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
+import { useState } from 'react'
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -22,10 +23,19 @@ const InputSubmit = styled.input`
     
 
 const Formulario = () => {
-    const [SelectMonedas]  = useSelectMonedas()
+  const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'GBP', nombre: 'Libra Esterlina'},
+]
+   const [moneda,SelectMonedas]  = useSelectMonedas('Elige tu Moneda', monedas)
+    
+    // const [state, setState] = useState()
   return (
     <form>
-         <InputSubmit 
+        <SelectMonedas />
+        <InputSubmit 
                     type="submit" 
                     value="Cotizar" 
                 />
